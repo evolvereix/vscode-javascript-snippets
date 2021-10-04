aee9df6c69c28d935e6d7f3472c0294f5a264d79
zen <0x11cb@gmail.com>
2021-10-04 17:21:29 +0800

    build: add release script

---
+++ scripts/release.js
@@ -0,0 +1,109 @@
+const args = require('minimist')(process.argv.slice(2))
+const fs = require('fs')
+const path = require('path')
+const execa = require('execa')
+const chalk = require('chalk')
+const semver = require('semver')
+const { prompt } = require('enquirer')
+const currentVersion = require('../package.json').version
+
+const isDryRun = true || args.dry
+
+const preId =
+  semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0]
+const inc = (i) => semver.inc(currentVersion, i, preId)
+
+const run = (bin, args, opts = {}) =>
+  execa(bin, args, { stdio: 'inherit', ...opts })
+const dryRun = (bin, args, opts = {}) =>
+  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
+const runIfNotDry = isDryRun ? dryRun : run
+
+const step = (msg) => console.log(chalk.cyan(msg))
+const versionIncrements = [
+  'patch',
+  'minor',
+  'major',
+  ...(preId ? ['prepatch', 'preminor', 'premajor', 'prerelease'] : [])
+]
+
+async function main() {
+  let targetVersion = args._[0]
+
+  if (!targetVersion) {
+    const { release } = await prompt({
+      type: 'select',
+      name: 'release',
+      message: 'Select release type',
+      choices: versionIncrements
+        .map((i) => `${i} (${inc(i)})`)
+        .concat(['custom'])
+    })
+
+    if (release === 'custom') {
+      targetVersion = (
+        await prompt({
+          type: 'input',
+          name: 'version',
+          message: 'Input custom version',
+          initial: currentVersion
+        })
+      ).version
+    } else {
+      targetVersion = release.match(/\((.*)\)/)[1]
+    }
+  }
+
+  if (!semver.valid(targetVersion)) {
+    throw new Error(`invalid target version: ${targetVersion}`)
+  }
+
+  const { yes } = await prompt({
+    type: 'confirm',
+    name: 'yes',
+    message: `Releasing v${targetVersion}. Confirm?`
+  })
+
+  if (!yes) return
+
+  step('\nUpdating version...')
+  updateVersions(targetVersion)
+
+  step('\nGenerate changelog...')
+  await run(`pnpm`, ['changelog'])
+
+  const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+  if (stdout) {
+    step('\nCommitting changes...')
+    await runIfNotDry('git', ['add', '-A'])
+    await runIfNotDry('git', ['commit', '-m', `${targetVersion}`])
+  } else {
+    console.log('No changes to commit.')
+  }
+
+  // push to GitHub
+  step('\nPushing to GitHub...')
+  await runIfNotDry('git', ['tag', `v${targetVersion}`])
+  await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
+  await runIfNotDry('git', ['push'])
+
+  if (isDryRun) {
+    console.log(`\nDry run finished - run git diff to see package changes.`)
+  }
+}
+
+function updateVersions(version) {
+  console.log(
+    '--path.resolve __dirname',
+    path.resolve(__dirname, '../package.json'),
+    version
+  )
+  const pkgPath = path.resolve(__dirname, '../package.json')
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  pkg.version = version
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+}
+
+main().catch((err) => {
+  console.error(err)
+})
