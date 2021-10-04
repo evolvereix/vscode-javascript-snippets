0ef5ae7896f0e16c88f28c0b342e4a3adbb1e2db
zen <0x11cb@gmail.com>
2021-10-04 17:21:29 +0800

    build: add release script

---
+++ scripts/checkInstallTool.js
@@ -0,0 +1,8 @@
+console.log('process.env.npm_execpath', process.env.npm_execpath)
+
+if (!/pnpm\.(c)?js$/.test(process.env.npm_execpath || '')) {
+  console.warn(
+    '\u001b[33mThis repository requires Yarn 1.x or pnpm for scripts to work properly.\u001b[39m\n'
+  )
+  process.exit(1)
+}
