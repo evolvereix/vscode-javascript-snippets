5a2724e1717293c49e7e151befe5816e10756d27
zen <0x11cb@gmail.com>
2022-05-10 01:10:48 +0800

    feat: add vue snippets

--- scripts/checkInstallTool.js
+++
@@ -1,6 +0,0 @@
-if (!/(pnpm|yarn)\.(c)?js$/.test(process.env.npm_execpath || '')) {
-  console.warn(
-    '\u001b[33mThis repository requires Yarn 1.x or pnpm for scripts to work properly.\u001b[39m\n'
-  )
-  process.exit(1)
-}
